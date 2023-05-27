67b48380cc5ac0d249f97d83f63d75d36abcc669
Zubaer <zobaeralam@iut-dhaka.edu>
2023-05-27 17:22:51 +0600

    Uploads: threeJS project create_pyramid.py pyramid.glb

---
+++ blender/scripts/create_pyramid.py
@@ -0,0 +1,38 @@
+import bpy
+
+# Clear all mesh objects
+bpy.ops.object.select_by_type(type='MESH')
+bpy.ops.object.delete()
+
+# Create a new mesh object
+mesh = bpy.data.meshes.new(name="PyramidMesh")
+obj = bpy.data.objects.new("Pyramid", mesh)
+
+# Link the object to the scene
+scene = bpy.context.scene
+scene.collection.objects.link(obj)
+
+# Create a pyramid
+verts = [(1, 1, 0), (1, -1, 0), (-1, -1, 0), (-1, 1, 0), (0, 0, 2)]  # 5 vertices
+faces = [(0, 1, 4), (1,  2, 4), ( 2,  3, 4), ( 3, 0, 4)]  # 4 faces
+
+# Update the mesh with the new data
+mesh.from_pydata(verts, [], faces)
+
+# Set the pyramid material
+material = bpy.data.materials.new(name="Pyramid Material")
+material.use_nodes = True
+nodes = material.node_tree.nodes
+node = nodes.get("Principled BSDF")
+
+# Set red color
+node.inputs["Base Color"].default_value = (10, 0, 0, 1)  
+
+# Make the material opaque
+material.blend_method = 'OPAQUE'  
+
+# Assign the material to the object
+obj.data.materials.append(material)
+
+# Export the model in GLTF format
+bpy.ops.export_scene.gltf(filepath="C:/Users/User/Desktop/TASK/evaluation/blender/models/pyramid.gltf")
---
+++ threejs/scripts/viewer.js
@@ -0,0 +1,55 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+
+var scene = new THREE.Scene();
+//adding ambient light
+scene.add(new THREE.AmbientLight(0xffffff));
+
+var camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.set(0, 0, 5);
+var renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x00ff00)
+document.body.appendChild(renderer.domElement);
+
+var gltf;
+
+// Load your 3D model
+var loader = new GLTFLoader();
+loader.load(
+    'models/pyramid.glb',
+    function (gltfData) {
+        // Assign the loaded gltf data to the gltf variable
+        gltf = gltfData;
+
+        // Scale down the model
+        //gltf.scene.scale.set(0.1, 0.1, 0.1);
+        scene.add(gltf.scene);
+    },
+    undefined,
+    function (error) {
+        console.error(error);
+    }
+);
+
+// Render the scene
+var animate = function () {
+    requestAnimationFrame(animate);
+
+    if (gltf) {
+        // Rotate the model if gltf is loaded
+        gltf.scene.rotation.y += 0.05; 
+        gltf.scene.rotation.x += 0.007; 
+    }
+
+    renderer.render(scene, camera);
+};
+
+animate();
